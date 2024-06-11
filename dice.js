25dea792a00a0c04eb02b99acdbc91742b15d8e0
Elizabeth Proctor <elizabethproctor66@gmail.com>
2024-06-10 20:26:29 -0700

    Some massive changes, shows the player who is going in red, displayed the dice, roll, and total buttons, fixed the new game button, a status check to make sure it is working.

--- dice.js
+++ dice.js
@@ -35,10 +35,8 @@ class Game {
         this.roundScore = 0;
         document.getElementById('total').value = '0';
         document.getElementById('current').textContent = 'Player ' + (this.activePlayer + 1);
-        // Add this line to display the current player's turn on the webpage
         document.getElementById('display_turn').textContent = 'It\'s Player ' + (this.activePlayer + 1) + '\'s turn';
     }
-    
 
     checkWin() {
         if (this.players[this.activePlayer].score >= 100) {
@@ -48,26 +46,33 @@ class Game {
     }
 }
 
-let player1 = new Player(0);
-let player2 = new Player(1);
-let game = new Game(player1, player2);
+window.onload = function() {
+    let player1 = new Player(0);
+    let player2 = new Player(1);
+    let game = new Game(player1, player2);
 
-document.getElementById('new_game').addEventListener('click', () => game = new Game(player1, player2));
+    document.getElementById('new_game').addEventListener('click', () => {
+        game = new Game(player1, player2);
+        document.getElementById('display_turn').textContent = 'Player 1 starts!';
+    });
 
-document.getElementById('roll').addEventListener('click', function() {
-    if(game.gamePlaying) {
-        let dice = game.rollDice();
-        if (dice !== 1) {
-            game.roundScore += dice;
-            document.getElementById('total').value = game.roundScore;
-        } else {
-            game.nextPlayer();
+    document.getElementById('roll').addEventListener('click', function() {
+        if(game.gamePlaying) {
+            let dice = game.rollDice();
+            if (dice !== 1) {
+                game.roundScore += dice;
+                document.getElementById('total').value = game.roundScore;
+            } else {
+                game.nextPlayer();
+            }
+        }    
+    });
+
+    document.getElementById('hold').addEventListener('click', function() {
+        if (game.gamePlaying) {
+            game.hold();
         }
-    }    
-});
+    });
 
-document.getElementById('hold').addEventListener('click', function() {
-    if (game.gamePlaying) {
-        game.hold();
-    }
-});
+    checkGameStatus(game);
+}
