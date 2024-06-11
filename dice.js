3dde081d0d469047422ba1837f5abd24fe86fad9
Elizabeth Proctor <elizabethproctor66@gmail.com>
2024-06-10 22:01:28 -0700

    Added CSS and fixed the font sizes.

--- dice.js
+++ dice.js
@@ -1,77 +1,90 @@
-var score1 = 0;
-var score2 = 0;
-var accumulatedScore = 0;
-var currentPlayer = 1;
-
-function startGame() {
-    var player1 = document.getElementById('player1').value;
-    var player2 = document.getElementById('player2').value;
-    
-    if (player1 === "" || player2 === "") {
-        alert("Please enter names for both players.");
-        return;
-    }
-
-    document.getElementById('turn').innerHTML = player1 + "'s turn";
-
-    alert(player1 + " and " + player2 + ", let's start the game!");
-}
-
-function newGame() {
-    score1 = 0;
-    score2 = 0;
-    accumulatedScore = 0;
-    currentPlayer = 1;
-    document.getElementById('score1').innerHTML = score1;
-    document.getElementById('score2').innerHTML = score2;
-    document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + accumulatedScore;
-    alert("New game started!");
-    startGame(); // Call startGame to reset player names and turn
-}
-
-function rollDie() {
-    var dieRoll = Math.floor(Math.random() * 6) + 1;
-    document.getElementById('dieRoll').innerHTML = "Die Roll: " + dieRoll;
-
-    if (dieRoll != 1) {
-        accumulatedScore += dieRoll;
-        document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + accumulatedScore;
-    } 
-
-    else {
-        accumulatedScore = 0;
-        document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + accumulatedScore;
-        alert("You rolled a 1, your turn is over!");
-        switchPlayer();
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.score = 0;
     }
 }
 
-function checkWin() {
-    if (score1 >= 100) {
-        document.getElementById('winMessage').innerHTML = document.getElementById('player1').value + " Wins!";
-        document.getElementById('winMessage').style.display = "block";
-    } else if (score2 >= 100) {
-        document.getElementById('winMessage').innerHTML = document.getElementById('player2').value + " Wins!";
-        document.getElementById('winMessage').style.display = "block";
+class Game {
+    constructor(player1, player2) {
+        this.players = [player1, player2];
+        this.currentPlayerIndex = 0;
+        this.turnScore = 0;
+    }
+
+    get currentPlayer() {
+        return this.players[this.currentPlayerIndex];
+    }
+
+    switchPlayer() {
+        this.currentPlayerIndex = 1 - this.currentPlayerIndex; // This will switch between 0 and 1
+        var player = this.currentPlayer.name;
+        document.getElementById('turn').innerHTML = player + "'s turn";
+    }
+
+    startGame() {
+        var player1 = document.getElementById('player1').value;
+        var player2 = document.getElementById('player2').value;
+        
+        if (player1 === "" || player2 === "") {
+            alert("Please enter names for both players.");
+            return;
+        }
+
+        this.players[0].name = player1;
+        this.players[1].name = player2;
+        document.getElementById('turn').innerHTML = player1 + "'s turn";
+
+        alert(player1 + " and " + player2 + ", let's start the game!");
+    }
+
+    newGame() {
+        this.players[0].score = 0;
+        this.players[1].score = 0;
+        this.turnScore = 0;
+        this.currentPlayerIndex = 0;
+        document.getElementById('score1').innerHTML = this.players[0].score;
+        document.getElementById('score2').innerHTML = this.players[1].score;
+        document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + this.turnScore;
+        alert("New game started!");
+        this.startGame(); // Call startGame to reset player names and turn
+    }
+
+    rollDie() {
+        var dieRoll = Math.floor(Math.random() * 6) + 1;
+        document.getElementById('dieRoll').innerHTML = "Die Roll: " + dieRoll;
+
+        if (dieRoll != 1) {
+            this.turnScore += dieRoll;
+            document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + this.turnScore;
+        } 
+
+        else {
+            this.turnScore = 0;
+            document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + this.turnScore;
+            alert("You rolled a 1, your turn is over!");
+            this.switchPlayer();
+        }
+    }
+
+    checkWin() {
+        if (this.players[0].score >= 100) {
+            document.getElementById('winMessage').innerHTML = this.players[0].name + " Wins!";
+            document.getElementById('winMessage').style.display = "block";
+        } else if (this.players[1].score >= 100) {
+            document.getElementById('winMessage').innerHTML = this.players[1].name + " Wins!";
+            document.getElementById('winMessage').style.display = "block";
+        }
+    }
+
+    hold() {
+        this.currentPlayer.score += this.turnScore;
+        document.getElementById('score' + (this.currentPlayerIndex + 1)).innerHTML = this.currentPlayer.score;
+        this.turnScore = 0;
+        document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + this.turnScore;
+        this.checkWin();
+        this.switchPlayer();
     }
 }
 
-function hold() {
-    if (currentPlayer == 1) {
-        score1 += accumulatedScore;
-        document.getElementById('score1').innerHTML = score1;
-    } else {
-        score2 += accumulatedScore;
-        document.getElementById('score2').innerHTML = score2;
-    }
-    accumulatedScore = 0;
-    document.getElementById('accumulatedScore').innerHTML = "Accumulated Score: " + accumulatedScore;
-    checkWin();
-    switchPlayer();
-}
-
-function switchPlayer() {
-    currentPlayer = 3 - currentPlayer; // This will switch between 1 and 2
-    var player = document.getElementById('player' + currentPlayer).value;
-    document.getElementById('turn').innerHTML = player + "'s turn";
-}
+var game = new Game(new Player(""), new Player(""));
